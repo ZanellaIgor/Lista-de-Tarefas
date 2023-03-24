adb543c08717fffd769cdca9d466786a5200983f
ZanellaIgor <igor_zanella@outlook.com>
2023-03-24 00:23:34 -0300

    modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react"
+import { useState } from "react"
 
 //css
 import styles from "./App.module.css"
@@ -10,24 +10,50 @@ import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
 
 //Interface
-import {ITask} from "./interfaces/Task";
+import { ITask } from "./interfaces/Task";
+import Modal from "./components/Modal";
 
 function App() {
+
   const [taskList, setTaskList] = useState<ITask[]>([])
+
+  const deleteTask = (id: number) => {
+    setTaskList(
+      taskList.filter(task => {
+        return task.id !== id
+      })
+    )
+  }
+
+  const hideOrShowModal = (display: boolean) => {
+    const modal = document.querySelector("#modal")
+    if(display){
+      modal!.classList.remove("hide")
+    } else{
+      modal!.classList.add("hide")
+    }
+  }
+const editTask = ():void => {
+  hideOrShowModal(true);
+
+}
+
   return (
     <div>
-      <Header/>
+      <Modal children={<TaskForm btnText="Editar Tarefa" taskList={taskList} />}
+      />
+      <Header />
       <main className={styles.main}>
-      <div>
-        <h2>Oque você vai fazer?</h2>
-        <TaskForm btnText="Criar Tarefa" taskList={taskList} setTaskList={setTaskList}/>
-      </div>
-      <div>
-        <h2>Tarefas</h2>
-        <TaskList taskList={taskList} />
-      </div>
+        <div>
+          <h2>Oque você vai fazer?</h2>
+          <TaskForm btnText="Criar Tarefa" taskList={taskList} setTaskList={setTaskList} />
+        </div>
+        <div>
+          <h2>Tarefas</h2>
+          <TaskList taskList={taskList} handleDelete={deleteTask} handleEdit={editTask}/>
+        </div>
       </main>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -8,30 +8,33 @@ import styles from "./TaskList.module.css"
 
 
 interface Props {
-
   taskList: ITask[]
+  handleDelete(id: number): void;
+  handleEdit(): void;
 }
 
-const TaskList = ({taskList}: Props) => {
+const TaskList = ({ taskList, handleDelete, handleEdit }: Props) => {
   return (
     <>
-    {taskList.length > 0 ? (
-      taskList.map((task) => (
-        <div key={task.id} className={styles.task}>
-          <div className={styles.details}>
-            <h4>{task.title}</h4>
-            <p>Dificuldade: {task.difficulty}</p>
+      {taskList.length > 0 ? (
+        taskList.map((task) => (
+          <div key={task.id} className={styles.task}>
+            <div className={styles.details}>
+              <h4>{task.title}</h4>
+              <p>Dificuldade: {task.difficulty}</p>
+            </div>
+            <div className={styles.actions}>
+              <i className='bi bi-pencil'
+                onClick={() => handleEdit()}></i>
+              <i className='bi bi-trash'
+                onClick={() => handleDelete(task.id)}></i>
+            </div>
           </div>
-          <div className={styles.actions}>
-            <i className='bi bi-pencil'></i>
-            <i className='bi bi-trash'></i>
-          </div>
-        </div>
-      ))
-    ) : (
-      <p>Não tem tarefas</p>
-    )
-    }
+        ))
+      ) : (
+        <p>Não tem tarefas</p>
+      )
+      }
 
     </>
   )
